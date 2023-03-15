0bc42a951e05cd1e8b8d62b1ab335aacf15332cb
eloesteone <barancewiczkacper@gmail.com>
2023-03-15 23:57:00 +0100

    Added function to delete task on double click

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-console.log("dupa");
-
 let text = document.getElementById("text");
 let addBtn = document.getElementById("add");
-let bottom = document.getElementById("bot")
+let bottom = document.getElementById("bot");
 
 addBtn.addEventListener("click", function(){
     bottom.style.display = "block";
     var paragraph = document.createElement('p');
+    paragraph.id = "task";
     paragraph.innerText = text.value;
     bottom.appendChild(paragraph);
+
+
+    paragraph.addEventListener("dblclick", function (){
+        bottom.removeChild(paragraph);
+
+        if (bottom.querySelectorAll('p').length === 0) {
+            bottom.style.display = 'none';
+        }
+    });
 });
+
+if (bottom.querySelectorAll('p').length === 0) {
+    bottom.style.display = 'none';
+}
